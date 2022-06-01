87b6417bb54fc60518c5f3eeff2c3ef2a1524c15
Carlos Tavera (Lord Shadow) <cjohnson2001xd@gmail.com>
2022-06-01 05:43:26 -0500

    Update MatrixV2.c

--- MatrixV2.c
+++ MatrixV2.c
@@ -1,119 +1,109 @@
 #include <stdio.h>
+
 int main()
 {
+    printf("======================================================\n");
+    printf("      Calculadora de Matrices Ax = B       Método LU  \n");
+    printf("                                                      \n");
+    printf("        Carlos Alberto Tavera Arteaga - 1628839A      \n");
+    printf("======================================================\n");
 
-  int n, i, j, k, l;
-  int L[10][10], U[10][10], A[10][10], b[10], x[10];
-  int sum, sum1, sum2, sum3;
+    int n, i, k, j, P;
+    double A[100][100], L[010][100] = {0}, U[100][100] = {0}, sum, B[100], z[100] = {0}, x[100] = {0};
 
-  printf("Enter the size of the matrix: ");
-  scanf("%d", &n);
+    printf("Introduce el tamaño de la matriz A\n");
 
-  printf("Enter the matrix A: \n");
-  for (i = 0; i < n; i++)
-  {
-    for (j = 0; j < n; j++)
+    scanf("%d", &n);
+    printf("Introduce los valores de la matriz A %d x %d:\n", n, n);
+    for (i = 1; i <= n; i++)
     {
-      scanf("%d", &A[i][j]);
+        for (j = 1; j <= n; j++)
+        {
+            scanf("%lf", &A[i][j]);
+        }
     }
-  }
 
-  printf("Enter the matrix b: \n");
-  for (i = 0; i < n; i++)
-  {
-    scanf("%d", &b[i]);
-  }
+    printf("\nIntroduce los valores B:\n", n);
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%lf", &B[i]);
+    }
 
-  // L matrix
-  for (i = 0; i < n; i++)
-  {
-    for (j = 0; j < n; j++)
-    {
-      if (i == j)
-      {
-        L[i][j] = 1;
-      }
-      else
-      {
-        L[i][j] = 0;
-      }
-    }
-  }
+    // Descomposicion LU
 
-  // U matrix
-  for (i = 0; i < n; i++)
-  {
-    for (j = 0; j < n; j++)
+    for (k = 1; k <= n; k++)
     {
-      if (i == j)
-      {
-        U[i][j] = A[i][j];
-      }
-      else
-      {
-        U[i][j] = 0;
-      }
-    }
-  }
+        U[k][k] = 1;
+        for (i = k; i <= n; i++)
+        {
+            sum = 0;
+            for (P = 1; P <= k - 1; P++)
+                sum += L[i][P] * U[P][k];
+            L[i][k] = A[i][k] - sum;
+        }
 
-  // L matrix
-  for (i = 0; i < n; i++)
-  {
-    for (j = 0; j < n; j++)
-    {
-      if (i > j)
-      {
-        L[i][j] = A[i][j];
-      }
+        for (j = k + 1; j <= n; j++)
+        {
+            sum = 0;
+            for (P = 1; P <= k - 1; P++)
+                sum += L[k][P] * U[P][j];
+            U[k][j] = (A[k][j] - sum) / L[k][k];
+        }
     }
-  }
 
-  // U matrix
-  for (i = 0; i < n; i++)
-  {
-    for (j = 0; j < n; j++)
+    // Mostrar matriz LU
+
+    printf("\nMatriz L:\n");
+    for (i = 1; i <= n; i++)
     {
-      if (i < j)
-      {
-        U[i][j] = A[i][j];
-      }
+        for (j = 1; j <= n; j++)
+            printf("%lf ", L[i][j]);
+        printf("\n");
     }
-  }
-  printf("The L matrix is: \n");
-  for (i = 0; i < n; i++)
-  {
-    for (j = 0; j < n; j++)
+    printf("\nMatriz U:\n");
+    for (i = 1; i <= n; i++)
     {
-      printf("%d ", L[i][j]);
+        for (j = 1; j <= n; j++)
+            printf("%lf ", U[i][j]);
+        printf("\n");
     }
-    printf("\n");
-  }
-  printf("The U matrix is: \n");
-  for (i = 0; i < n; i++)
-  {
-    for (j = 0; j < n; j++)
+
+    // Sustitucion hacia adelante
+
+    for (i = 1; i <= n; i++)
     {
-      printf("%d ", U[i][j]);
+        sum = 0;
+        for (P = 1; P < i; P++)
+            sum += L[i][P] * z[P];
+        z[i] = (B[i] - sum) / L[i][i];
+    }
+
+    // Calcular X
+
+    for (i = n; i > 0; i--)
+    {
+        sum = 0;
+        for (P = n; P > i; P--)
+            sum += U[i][P] * x[P];
+        x[i] = (z[i] - sum) / U[i][i];
+    }
+
+    printf("\nValores de X:\n");
+
+    for (i = 1; i <= n; i++)
+    {
+        printf("x%d= %lf\n", i, x[i]);
     }
     printf("\n");
-  }
-  
-  //solve x vector
-  for (i = 0; i < n; i++)
-  {
-    sum = 0;
-    for (j = 0; j < i; j++)
-    {
-      sum = sum + L[i][j] * x[j];
-    }
-    x[i] = (b[i] - sum) / L[i][i];
-  }
-  printf("The x vector is: \n");
-  for (i = 0; i < n; i++)
-  {
-    printf("%d ", x[i]);
-  }
-  printf("\n");
-  
-  
+    printf("=====================================================\n");
+    return 0;
 }
+
+/*
+3 6 9
+6 2 1
+9 1 2
+a
+2 8 5
+*/
+
